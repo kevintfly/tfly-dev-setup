4794473f8a73ff9ddfa56cea8fe72518f953f86e
kevin <kevintfly@gmail.com>
2014-08-10 21:55:13 -0700

    blank cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'tfly-dev-setup'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tfly-dev-setup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
