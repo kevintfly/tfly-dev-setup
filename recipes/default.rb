c8535b48d2e51eb8315f0e94b016847d20b588f0
kevin <kevintfly@gmail.com>
2014-08-10 21:56:49 -0700

    change versions

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute 'change default grails version' do
+  user node['current-user']
+  command "cd /usr/local/Cellar && git checkout 9312992 /usr/local/Library/Formula/grails.rb"
+end
+
+execute 'change default thrift version' do
+  user node['current-user']
+  command "cd /usr/local/Cellar && git checkout eccc96b /usr/local/Library/Formula/thrift.rb"
+end
